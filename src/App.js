53ac3bfdd4f2a6d912327d14a2cb10f2fdbec41a
Matt Pike <mattp91@gmail.com>
2023-08-04 16:09:29 -0400

    Finished app

--- src/App.js
+++ src/App.js
@@ -1,62 +1,49 @@
 import "./App.css";
 
 function App() {
-  const intro =
-    "Full-stack web developer. While coding, I love coffee! When not coding (still coffee!), I love to spend time with my wife and daughter. If I'm with the guys, I enjoy poker, board games, beer, whiskey, and theology.";
-  const skills = [
-    "HTML+CSS",
-    "JavaScript",
-    "Web Design",
-    "Git and GitHub",
-    "React",
-    "Node",
-  ];
-
   return (
     <div className="card">
-      <Avatar photoName="profilePNG.png" />
+      <Avatar />
       <div className="data">
-        <Intro name="Matt Pike" text={intro} />
-        <SkillList skills={skills} />
+        <Intro />
+        <SkillList />
       </div>
     </div>
   );
 }
 
 function Avatar(props) {
-  return <img className="avatar" src={props.photoName} alt="Avatar"></img>;
+  return <img className="avatar" src="profilePNG.png" alt="Matt Pike"></img>;
 }
 
 function Intro(props) {
   return (
-    <>
-      <h1>{props.name}</h1>
-      <p>{props.text}</p>
-    </>
+    <div>
+      <h1>Matt Pike</h1>
+      <p>
+        Full-stack web developer. While coding, I love coffee! When not coding
+        (still coffee!), I love to spend time with my wife and daughter. If I'm
+        with the guys, I enjoy poker, board games, beer, whiskey, and theology.
+      </p>
+    </div>
   );
 }
 
 function SkillList(props) {
   return (
-    <div className="skillList">
-      <span className="skill" style={{ backgroundColor: "lightblue" }}>
-        {props.skills[0]}
-      </span>
-      <span className="skill" style={{ backgroundColor: "red" }}>
-        {props.skills[1]}
-      </span>
-      <span className="skill" style={{ backgroundColor: "lightgreen" }}>
-        {props.skills[2]}
-      </span>
-      <span className="skill" style={{ backgroundColor: "lightpink" }}>
-        {props.skills[3]}
-      </span>
-      <span className="skill" style={{ backgroundColor: "orange" }}>
-        {props.skills[4]}
-      </span>
-      <span className="skill" style={{ backgroundColor: "yellow" }}>
-        {props.skills[5]}
-      </span>
+    <div className="skill-list">
+      <Skill skill="React" color="blue" />
+      <Skill skill="JavaScript" color="orange" />
+      <Skill skill="React" color="purple" />
+      <Skill skill="Node" color="green" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
     </div>
   );
 }
